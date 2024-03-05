531c7f612aa018ec783692419af588b280a077c0
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2024-03-04 21:37:10 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
     <div>
-      <p className="bg-violet-700  text-violet-800 text-sm">Hello world</p>
+      <p className="text-violet-800 text-sm">Hello world</p>
     </div>
   );
 }
